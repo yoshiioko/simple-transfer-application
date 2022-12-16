b138878b982baa3b6d9785e74c751a2416b58b47
yoshiioko <acardoza@pdx.edu>
2022-12-15 22:37:53 -0800

    Enable sender + receiver keys to be read from .env file

--- src/index.ts
+++ src/index.ts
@@ -7,32 +7,48 @@ dotenv.config();
 async function initializeSender(
   connection: Web3.Connection
 ): Promise<Web3.Keypair> {
-  console.log("Generating Sender Keypair... 🗝️");
-  const keypair = Web3.Keypair.generate();
+  if (!process.env.SENDER_PRIVATE_KEY) {
+    console.log("Generating Sender Keypair... 🗝️");
+    const keypair = Web3.Keypair.generate();
 
-  console.log("Creating .env file");
-  fs.writeFileSync(
-    ".env",
-    `SENDER_PRIVATE_KEY=[${keypair.secretKey.toString()}]\r\n`
-  );
+    console.log("Creating .env file");
+    fs.writeFileSync(
+      ".env",
+      `SENDER_PRIVATE_KEY=[${keypair.secretKey.toString()}]\r\n`
+    );
 
-  return keypair;
+    return keypair;
+  }
+
+  const secret = JSON.parse(process.env.SENDER_PRIVATE_KEY ?? "") as number[];
+  const secretKey = Uint8Array.from(secret);
+  const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
+
+  return keypairFromSecret;
 }
 
 async function initializeReceiver(
   connection: Web3.Connection
 ): Promise<Web3.Keypair> {
-  console.log("Generating Receiver Keypair... 🗝️");
-  const keypair = Web3.Keypair.generate();
+  if (!process.env.RECEIVER_PRIVATE_KEY) {
+    console.log("Generating Receiver Keypair... 🗝️");
+    const keypair = Web3.Keypair.generate();
 
-  console.log("Creating .env file");
-  fs.writeFileSync(
-    ".env",
-    `RECEIVER_PRIVATE_KEY=[${keypair.secretKey.toString()}]`,
-    { flag: "a+" }
-  );
+    console.log("Creating .env file");
+    fs.writeFileSync(
+      ".env",
+      `RECEIVER_PRIVATE_KEY=[${keypair.secretKey.toString()}]`,
+      { flag: "a+" }
+    );
 
-  return keypair;
+    return keypair;
+  }
+
+  const secret = JSON.parse(process.env.RECEIVER_PRIVATE_KEY ?? "") as number[];
+  const secretKey = Uint8Array.from(secret);
+  const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
+
+  return keypairFromSecret;
 }
 
 async function main() {
